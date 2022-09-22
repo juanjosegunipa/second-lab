b1e961259c03425c0b5b7f479eaf01ecf6c22c89
juanjguanipaf@gmail.com <juanjguanipaf@gmail.com>
2022-09-22 13:22:33 -0400

    second commit

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -4,6 +4,7 @@ const user = require('../models/User.model');
 const account = require('../models/Account.model')
 const transaction = require('../models/Transaction.model');
 const { isAuthenticated, isNotAuthenticated } = require('../middlewares/auth.middlewares');
+const { findById } = require('../models/User.model');
 
 
 router.get('/profile/deposit', (req, res, next) => {
@@ -17,19 +18,28 @@ router.get('/profile/deposit', (req, res, next) => {
 
 router.post('/profile/deposit', (req, res, next) => {
     const addBalance = Number(req.body.deposit);
-    account.findById(req.body.account)
-        .then(foundAccount => {
-            account.findByIdAndUpdate(req.body.account, {
-                balance: addBalance + foundAccount.balance
-            }, { new: true })
-                .then(response => {
-                    console.log(response);
-                    res.redirect('/profile');
-                })
-        })
-        .catch(err => {
-            res.render(err);
-        })
+    if (addBalance >= 0) {
+        account.findById(req.body.account)
+            .then(foundAccount => {
+                account.findByIdAndUpdate(req.body.account, {
+                    balance: addBalance + foundAccount.balance
+                }, { new: true })
+                    .then(response => {
+                        console.log(response);
+                        res.redirect('/profile');
+                    })
+            })
+            .catch(err => {
+                res.render(err);
+            })
+    } else {
+        user.findById(req.session.user._id)
+            .populate('accounts')
+            .then(foundAccount => {
+                console.log(foundAccount)
+                res.render('deposit.hbs', { foundAccount: foundAccount.accounts, errorMessage: 'asdf' })
+            })
+    }
 })
 
 router.get('/profile/open-account', (req, res, next) => {
@@ -59,59 +69,68 @@ router.post('/profile/open-account', (req, res, next) => {
 })
 
 router.get('/profile/accounts/:accountId', (req, res, next) => {
-    console.log('Route parameters', req.params)
+    // console.log('Route parameters', req.params)
     account.findById(req.params.accountId)
-        .populate('transaction')
-        .then(foundAccount => {
-            // console.log('transactions found:' + foundAccount.transaction[0]._id);
-            for (let i = 0; i < foundAccount.transaction.length; i++) {
-                // console.log('HERE ' + foundAccount.transaction[i].sendingAccount, req.params.accountId)
-                if (String(foundAccount.transaction[i].sendingAccount) === String(req.params.accountId)) {
-                    account.findById(foundAccount.transaction[i].sendingAccount)
-                        .then(accountsSending => {
-                            console.log('account sending: ' + accountsSending)
-                            user.findById(accountsSending.userId)
-                                .then(userSending => {
-                                    console.log('user sending: ' + userSending.name, userSending.lastName, userSending)
-                                    account.findById(foundAccount.transaction[i].receivingAccount)
-                                        .then(accountsReceiving => {
-                                            // console.log('receiving occounts' + accountsReceiving)
-                                            user.findById(accountsReceiving.userId)
-                                                .then(userReceiving => {
-                                                    console.log('user receiving: ' + userReceiving.name, userReceiving.lastName, userReceiving)
-                                                    res.render('account-details.hbs', {
-                                                        userSending, userReceiving,
-                                                        accountName: foundAccount.accountName,
-                                                        balance: foundAccount.balance,
-                                                        transactionArray: [
-                                                            foundAccount.transaction.reverse(),
-                                                            {
-                                                                userReceiving: {
-                                                                    name: userReceiving.name,
-                                                                    lastName: userReceiving.lastName
-                                                                }
-                                                            },
-                                                            {
-                                                                userSending: {
-                                                                    name: userSending.name,
-                                                                    lastName: userSending.lastName
-                                                                }
-                                                            }]
-                                                    })
-                                                })
-                                        })
-                                })
-                        })
-                    return;
+        .populate({
+            path: 'transaction',
+            populate: {
+                path: 'sendingAccount receivingAccount',
+                model: 'Account',
+                populate: {
+                    path: 'userId',
+                    model: 'User'
                 }
             }
-            // res.render('account-details.hbs', {
-            //     accountName: foundAccount.accountName,
-            //     balance: foundAccount.balance,
-            //     transactionArray: foundAccount.transaction.reverse()
-            // })
+        })
+        .then(foundAccount => {
+            // console.log(foundAccount, 'Found account!!!!!')
+            res.render('account-details.hbs', {
+                _id: foundAccount._id,
+                accountName: foundAccount.accountName,
+                balance: foundAccount.balance,
+                transactionArray: foundAccount.transaction.reverse()
+            })
         })
         .catch(err => console.log(err))
+})
+
+router.get('/profile/accounts/:_id/delete', (req, res, next) => {
+    const accountId = req.params._id
+
+    account.findById(accountId)
+        .populate({
+            path: 'transaction',
+            populate: {
+                path: 'sendingAccount receivingAccount',
+                model: 'Account',
+                populate: {
+                    path: 'userId',
+                    model: 'User'
+                }
+            }
+        })
+        .then(foundAccount => {
+            // console.log(foundAccount, 'Found account')
+            if (foundAccount.amount == 0 || !foundAccount.transaction.length) {
+                account.findByIdAndDelete(accountId)
+                    .then(response => {
+                        console.log(response)
+                        res.redirect('/profile')
+                    })
+            } else {
+                console.log('Can not delete this account')
+                console.log(foundAccount, "found account!dfgergwsrgergerger")
+                res.render('account-details.hbs', {
+                    _id: foundAccount._id,
+                    accountName: foundAccount.accountName,
+                    balance: foundAccount.balance,
+                    transactionArray: foundAccount.transaction.reverse(),
+                    errorMessage: 'asdf'
+                })
+            }
+        })
+
+        .catch(err => console.log(err, 'this is the error'))
 })
 
 router.get('/profile/send-money', (req, res, next) => {
@@ -158,33 +177,58 @@ router.post('/profile/send-money/confirmation/:userId', (req, res, next) => {
     const currentAccount = req.body.accountSending;
     const receivingAccount = req.body.accountReceiving;
 
-    transaction.create({
-        sendingAccount: currentAccount,
-        receivingAccount: receivingAccount,
-        amount: myBalance
-    })
-        .then(savedTransaction => {
-            account.findById(currentAccount)
-                .then(firstAccount => {
-                    account.findById(receivingAccount)
-                        .then(secondAccount => {
-                            account.findByIdAndUpdate(firstAccount._id, { $push: { transaction: savedTransaction._id }, balance: Number(firstAccount.balance) - Number(myBalance) }, { new: true })
-                                .then(response => {
-                                    console.log('1st response:' + response)
-                                    account.findByIdAndUpdate(secondAccount._id, { $push: { transaction: savedTransaction._id }, balance: Number(secondAccount.balance) + Number(myBalance) }, { new: true })
-                                        .then(secondResponse => {
-                                            console.log('2nd response' + secondResponse)
-                                            user.findById(secondAccount.userId)
-                                                .then(foundUser => {
-                                                    console.log('this is the found user: ' + foundUser)
-                                                    res.render('money-sent.hbs', { savedTransaction, foundUser, myBalance })
+    if (myBalance > 0) {
+
+        transaction.create({
+            sendingAccount: currentAccount,
+            receivingAccount: receivingAccount,
+            amount: myBalance
+        })
+            .then(savedTransaction => {
+                account.findById(currentAccount)
+                    .then(firstAccount => {
+                        if (firstAccount.balance >= myBalance) {
+                            account.findById(receivingAccount)
+                                .then(secondAccount => {
+                                    account.findByIdAndUpdate(firstAccount._id, { $push: { transaction: savedTransaction._id }, balance: Number(firstAccount.balance) - Number(myBalance) }, { new: true })
+                                        .then(response => {
+                                            console.log('1st response:' + response)
+                                            account.findByIdAndUpdate(secondAccount._id, { $push: { transaction: savedTransaction._id }, balance: Number(secondAccount.balance) + Number(myBalance) }, { new: true })
+                                                .then(secondResponse => {
+                                                    console.log('2nd response' + secondResponse)
+                                                    user.findById(secondAccount.userId)
+                                                        .then(foundUser => {
+                                                            console.log('this is the found user: ' + foundUser)
+                                                            res.render('money-sent.hbs', { savedTransaction, foundUser, myBalance })
+                                                        })
                                                 })
                                         })
                                 })
-                        })
-                })
-        })
-        .catch(err => console.log(err))
+                        } else {
+                            res.render('insufficient-founds.hbs', {
+                                BalanceErrorMessage: 'asdf'
+                            })
+                        }
+                    })
+            })
+            .catch(err => console.log(err))
+    } else {
+        //     const currentUser = req.session.user._id;
+        //     console.log(req.params.userId)
+        //     user.findById(currentUser)
+        //         .then(receiver => {
+        //             user.findById(currentUser)
+        //                 .populate('accounts')
+        //                 .then(foundAccount => {
+        //                     user.findById(receiver._id)
+        //                         .populate('accounts')
+        //                         .then(foundReceiverAccount => {
+        res.render('send-negative.hbs', { errorMessage: 'asdf' })
+        //                         })
+        //                 })
+        //         })
+
+    }
 })
 
 module.exports = router;
